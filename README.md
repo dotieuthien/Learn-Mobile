e630b302251131c445930bc93ab900a48a244ef4
dotieuthien <dotieuthien9997@gmail.com>
2022-09-04 14:10:44 +0700

    var

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Learn-Mobile
+
+This repository is my note for Flutter course. 
+
+I want to build a simple mobile app and deploy deep learning model to mobile.
--- dart/test.dart
+++ dart/test.dart
@@ -1,6 +1,27 @@
 void main() {
-    var name = "tieuthien";
-    print(name);
-    String lastName = "do";
-    print(lastName);
+  var name = "tieuthien";
+  print(name);
+  String lastName = "do";
+  print(lastName);
+
+  const y = [100];
+  // y.add(1);
+  print(y);
+
+  final x = [100];
+  x.add(1000);
+  print(x);
+
+  // Test class
+  var thien = Person(25);
+  print(thien);
+}
+
+
+class Person {
+  // const have to use with static and init a value
+  static const String fullName = "dotieuthien";
+  // use final without init
+  final int age;
+  Person(this.age);
 }
